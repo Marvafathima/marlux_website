6d869265f0f29c47b8b7130dfa7797f426418ee1
Marvafathima <marvafathima62@gmail.com>
2024-03-17 21:55:48 +0530

    chart for best sellers done

--- adminapp/views.py
+++ adminapp/views.py
@@ -495,9 +495,9 @@ import datetime
 def top_selling_product(request):
     today=datetime.date.today()
 
-    print(today)
+    
     period=request.POST.get('period')
-    print(period)
+  
     if period=="year":
         start_date=today.replace(month=1,day=1)
     elif period=="month":
@@ -519,10 +519,7 @@ def top_selling_product(request):
         .values('product_variant__prod_id__brand_id__br_name')\
         .annotate(total_quantity=Count('id'))\
         .order_by('-total_quantity')[:5]
-    print("top selling coorect")
-    print(top_selling_products)
-    print(top_selling_category)
-    print(top_selling_brand)
+   
     labels = [item['product_variant__prod_id__pr_name'] for item in top_selling_products]
     data = [item['total_quantity'] for item in top_selling_products]
     labels1 = [item['product_variant__prod_id__subcat_id__sub_name'] for item in top_selling_category]
@@ -539,7 +536,33 @@ def top_selling_product(request):
       
 @staff_member_required
 def top_seller(request):
-    return render(request,'top_seller.html')
+    start_date=datetime.date.today()
+    orders = Order.objects.filter(created_at__gte=start_date)
+    top_selling_products = OrderProduct.objects.filter(order__in=orders)\
+        .values('product_variant__prod_id__pr_name')\
+        .annotate(total_quantity=Count('id'))\
+        .order_by('-total_quantity')[:5]
+    top_selling_category= OrderProduct.objects.filter(order__in=orders)\
+        .values('product_variant__prod_id__subcat_id__sub_name')\
+        .annotate(total_quantity=Count('id'))\
+        .order_by('-total_quantity')[:5]
+    top_selling_brand= OrderProduct.objects.filter(order__in=orders)\
+        .values('product_variant__prod_id__brand_id__br_name')\
+        .annotate(total_quantity=Count('id'))\
+        .order_by('-total_quantity')[:5]
+   
+    labels = [item['product_variant__prod_id__pr_name'] for item in top_selling_products]
+    data = [item['total_quantity'] for item in top_selling_products]
+    labels1 = [item['product_variant__prod_id__subcat_id__sub_name'] for item in top_selling_category]
+    data1 = [item['total_quantity'] for item in top_selling_category]
+    labels2 = [item['product_variant__prod_id__brand_id__br_name'] for item in top_selling_brand]
+    data2 = [item['total_quantity'] for item in top_selling_brand]
+    return render(request,'top_seller.html',{
+                        'labels': labels, 'data': data,
+                        'labels1': labels1, 'data1': data1,
+                        'labels2': labels2, 'data2': data2,
+
+    })
 
 
 
